57471514d67230d2b1800b5f41be88e06d533b89
swatitoshniwal <swati.toshniwal2@gmail.com>
2020-04-26 13:11:59 +0530

    Added SQL commands to create schema="quora". Made changes in the sql files.

--- quora-service/src/main/java/com/upgrad/quora/service/entity/UserAuthEntity.java
+++ quora-service/src/main/java/com/upgrad/quora/service/entity/UserAuthEntity.java
@@ -10,7 +10,7 @@ import java.io.Serializable;
 import java.util.Date;
 
 @Entity
-@Table(name = "user_auth", schema = "public")
+@Table(name = "user_auth", schema = "quora")
 @NamedQueries(
         {
 //                Enter you named queries like in UserEntity.java file
